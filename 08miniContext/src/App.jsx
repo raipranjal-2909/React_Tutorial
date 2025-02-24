d5857d69595c758143c4d948a829f892145cdccc
Pranjal Rai <pranjalrai2909@gmail.com>
2025-02-24 14:27:29 +0530

    Context api

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,17 @@
+import UserContextProvider from './Context/UserContextProvider'
+import './App.css'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>React with Chai</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
