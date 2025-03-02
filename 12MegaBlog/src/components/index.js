035bbf8c358981ed63a8a9f12ef9e1e3312895b6
Pranjal Rai <pranjalrai2909@gmail.com>
2025-03-02 19:55:09 +0530

    Mega Blog App

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,19 @@
+import Header from "./Headers/Header";
+import Footer from "./Footers/Footer";
+import Container from './container/container';
+import Logo from "./Logo";
+import LogoutBtn from "./Headers/LogoutBtn";
+import Button from './Button'
+import Input from './Input'
+import RTE from "./RTE";
+
+export {
+    Header,
+    Footer,
+    Container, 
+    Logo, 
+    LogoutBtn,
+    Button,
+    Input,
+    RTE
+}
